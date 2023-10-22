5e0a971e96ded25dd007d9fb4292535ae164abb9
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2023-10-22 09:52:57 -0500

    Added 3x3 structure for game board GUI

--- index.py
+++ index.py
@@ -6,6 +6,8 @@ Creation date: October 21, 2023
 
 import numpy as np
 import pandas as pd
+from tkinter import *
+from tkinter import ttk
 
 
 class Board():
@@ -19,13 +21,22 @@ class Board():
         print(self.tiles)
 
 
+def openGame():
+    window = Tk()
+    for i in range(3):
+        window.columnconfigure(i, weight=1, minsize=75)
+        window.rowconfigure(i, weight=1, minsize=50)
+        for j in range(3):
+            frame = ttk.Frame(window, relief=RAISED, borderwidth=5)
+            frame.grid(row=i, column=j)
+            ttk.Button(frame, text=f"Row {i}\nColumn {j}").pack()
+
+    window.mainloop()
+
+
 def main():
     board = Board()
-    board.show()
-    board.play(0, "x")
-    board.show()
-    board.play(2, "o")
-    board.show()
+    openGame()
 
 
 if __name__ == "__main__":
