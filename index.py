7377abaf5cf1c01958d6a442843bcf6a7697f2d8
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2023-10-22 16:37:42 -0500

    Moved Board class to its own module

--- index.py
+++ index.py
@@ -10,77 +10,7 @@ from tkinter import *
 from tkinter import ttk
 from keras.layers import Dense, Input
 from keras import Model
-
-
-class Board():
-
-    def __init__(self, firstPlayer, secondPlayer):
-        self.tiles = np.zeros((9,2), dtype=bool)
-        self.__thisPlayer = firstPlayer
-        self.__otherPlayer = secondPlayer
-        self.__winningCombos = [[1, 1, 1, 0, 0, 0, 0, 0, 0],
-                                [0, 0, 0, 1, 1, 1, 0, 0, 0],
-                                [0, 0, 0, 0, 0, 0, 1, 1, 1],
-                                [1, 0, 0, 1, 0, 0, 1, 0, 0],
-                                [0, 1, 0, 0, 1, 0, 0, 1, 0],
-                                [0, 0, 1, 0, 0, 1, 0, 0, 1],
-                                [1, 0, 0, 0, 1, 0, 0, 0, 1],
-                                [0, 0, 1, 0, 1, 0, 1, 0, 0]]
-        self.__gameStatus = "Unresolved"
-        self.__winner = "TBD"
-        self.__allBoards = []
-
-    def play(self, tile, player):
-        if self.__gameStatus == "Resolved":
-            print("Game is over.", self.__winner, "wins!\nPlease reset board to play again.")
-        elif self.__thisPlayer != player:
-            print("It is not your turn. Please let the other player play their turn.")
-        elif np.sum(self.tiles[tile]) > 0:
-            print("Space is already occupied. Cannot play there.")
-        else:
-            self.tiles[tile, player] = 1
-            self.__switchPlayersTurn()
-            self.__allBoards.append(self.tiles.copy())  # important to append a copy of the current game board, so it does not get updated in the future
-            self.analyze(player)
-
-    def __switchPlayersTurn(self):
-        temp = self.__thisPlayer
-        self.__thisPlayer = self.__otherPlayer
-        self.__otherPlayer = temp
-
-    def show(self):
-        print(self.tiles)
-
-    def analyze(self, player):
-        thisPlayersBoard = self.tiles[:,player]
-        for combo in self.__winningCombos:
-            test = np.isin(np.where(combo), np.where(thisPlayersBoard))
-            result = test.prod()
-            if result == 1:
-                self.__gameStatus = "Resolved"
-                self.__winner = player
-                print("Game over.", player, "wins!")
-                print("Final board:")
-                print(self.show())
-                break
-
-    def reset(self):
-        self.__init__(self.__thisPlayer, self.__otherPlayer)
-
-    def getStatus(self):
-        return self.__gameStatus
-
-    def getAllBoards(self):
-        return self.__allBoards
-
-    def getPlayersTurn(self):
-        return self.__thisPlayer
-
-    def getWinner(self):
-        return self.__winner
-
-    def getWinningCombos(self):
-        return self.__winningCombos
+from board import Board
 
 
 def openGame():
